e408f2b7ff46c8ea5506fceaeb10e6da0a6f68dd
manatee <bolonin.lex@yandex.ru>
2024-07-18 16:00:35 +0300

    BurgerIngredients component added

--- src/components/burger-ingredients/BurgerIngredients.tsx
+++ src/components/burger-ingredients/BurgerIngredients.tsx
@@ -2,7 +2,26 @@ import React from "react";
 import { Counter, CurrencyIcon, Tab, } from "@ya.praktikum/react-developer-burger-ui-components";
 import styles from "./BurgerIngredients.module.css"
 
-export default function BurgerIngredients() {
+
+export default function BurgerIngredients({ ingredientsDetails }) {
+    const bunsDetails = ingredientsDetails.filter(ingredient => ingredient.type === "bun");
+    const mainDetails = ingredientsDetails.filter(ingredient => ingredient.type === "main");
+    const sauceDetails = ingredientsDetails.filter(ingredient => ingredient.type === "sauce");
+
+    function IngredientCards({ filteredIngredientsDetails }) {
+        return filteredIngredientsDetails.map(ingredient => {
+            return (
+                <div className={`${styles.ingredient_card_details}`}>
+                    <img src={ingredient.image} alt={`${ingredient.name} preview`} className="ml-4 mr-4" />
+                    <div className={`${styles.currency}`}>
+                        <span className="text text_type_main-default">{ingredient.price}</span>
+                        <CurrencyIcon type="primary"/>
+                    </div>
+                    <span className="text text_type_main-default">{ingredient.name}</span>
+                </div>
+            )
+        })
+    }
     const [current, setCurrent] = React.useState('one')
     return (
         <section className={`${styles.burger_ingredients_container} pt-10`}>
@@ -20,6 +39,26 @@ export default function BurgerIngredients() {
                     Начинки
                 </Tab>
             </div>
+            <ul className={`${styles.ingredients_list} pt-10 custom-scroll`}>
+                <li className={`${styles.ingredient_card_container} pb-10`}>
+                    <h2 className="text text_type_main-medium">Булки</h2>
+                    <div className={`${styles.ingredient_card}`}>
+                        <IngredientCards filteredIngredientsDetails={bunsDetails}/>
+                    </div>
+                </li>
+                <li className={`${styles.ingredient_card_container} pb-10`}>
+                    <h2 className="text text_type_main-medium">Соусы</h2>
+                    <div className={`${styles.ingredient_card}`}>
+                        <IngredientCards filteredIngredientsDetails={sauceDetails}/>
+                    </div>
+                </li>
+                <li className={`${styles.ingredient_card_container} pb-10`}>
+                    <h2 className="text text_type_main-medium">Начинки</h2>
+                    <div className={`${styles.ingredient_card}`}>
+                        <IngredientCards filteredIngredientsDetails={mainDetails}/>
+                    </div>
+                </li>
+            </ul>
         </section>
     )
 }
