313a07bb2a7ad055119b954ab50fffea1e496976
manatee <bolonin.lex@yandex.ru>
2024-07-23 11:31:11 +0300

    Removed hardcoded data, contributed API

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,18 +1,34 @@
 import React from 'react';
-import ingredientsDetails from '../../utils/data';
 import styles from './App.module.css';
 import AppHeader from '../app-header/AppHeader';
 import BurgerIngredients from '../burger-ingredients/BurgerIngredients';
 import BurgerConstructor from '../burger-constructor/BurgerConstructor';
 
+const SERVER_URL = "https://norma.nomoreparties.space/api/ingredients"
+
 function App() {
+  const [ingredientsDetails, setIngredientsDetails] = React.useState([])
+
+  React.useEffect(() => {
+    try {
+      fetch(SERVER_URL)
+        .then(res => res.json())
+        .then(data => setIngredientsDetails(data.data))
+    }
+    catch(e) {
+      console.log("Ошибка запроса на сервер")
+    }
+  }, []);
+
   return (
     <>
       <AppHeader />
-      <main className={styles.main_container}>
-        <BurgerIngredients ingredientsDetails={ingredientsDetails}/>
-        <BurgerConstructor ingredientsDetails={ingredientsDetails}/>
-      </main>
+      {ingredientsDetails.length > 0 &&
+        <main className={styles.main_container}>
+          <BurgerIngredients ingredientsDetails={ingredientsDetails}/>
+          <BurgerConstructor ingredientsDetails={ingredientsDetails}/>
+        </main>
+      }
     </>
   );
 }
