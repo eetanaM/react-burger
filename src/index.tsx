55143daf5bae7245e2601d6273c5f53e4f78acee
manatee <bolonin.lex@yandex.ru>
2024-07-31 12:46:31 +0300

    Redux added

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/app/App';
 import reportWebVitals from './reportWebVitals';
+import { compose, createStore, applyMiddleware } from 'redux';
+import '@redux-devtools/extension';
+import { rootReducer } from './reducers';
+
+
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
+
+const enhancer = composeEnhancers();
+
+const store = createStore(rootReducer, enhancer);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
